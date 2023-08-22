57ca3387ffd1b527181efa6487d38f40ccf6007f
hieu.trong.mai <hieu.trong.mai@sun-asterisk.com>
2023-08-23 01:43:13 +0700

    abc

--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -18,7 +18,7 @@ export const messageTelegram = (content: string, profile?: IProfile) => {
   //   //add profile url
   //   message += `Check out profile [here](${BASE_ENDPOINT.PROFILE_URL}${profile.uid})`;
   // }
-  bot.telegram.sendMessage(TELEGRAM_CHANNEL_ID, message, {
+  bot.telegram.sendMessage(TELEGRAM_CHANNEL_ID_LADUY, message, {
     parse_mode: "Markdown",
   });
 
